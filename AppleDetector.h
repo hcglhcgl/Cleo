e3fc08af26924835ad730eae844a9206173d6013
Patrik <rekech19@gmail.com>
2022-04-08 13:54:29 +0200

    vision - getCoordinates

--- AppleDetector.h
+++ AppleDetector.h
@@ -6,11 +6,15 @@
 
 using namespace std;
 using namespace cv;
-
 class AppleDetector {
 
 public:
+	vector<Vec3f> circles;
 	AppleDetector();
+	vector<Vec3f> getCircles(Mat tresholded);
+	Mat getCirclesMask(Mat gray, bool small);
 	Mat findWhiteApples(Mat image);
-	Mat findOrangeApples(Mat image);
+	Mat findOrangeApples(Mat image, bool small);
+	Mat findOrangeApples2(Mat image);
+	Vec3i getOrangeAppleCoordinates(Mat image);
 };
